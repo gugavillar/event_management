55a2918ada88c11a4077c98b3058d9efb907eab9
Guga Villar <grsv.21@gmail.com>
2024-06-30 15:06:02 -0300

    :sparkles: Select component

---
+++ src/components/Atoms/Select/Select.test.tsx
@@ -0,0 +1,98 @@
+import { faker } from '@faker-js/faker'
+import { fireEvent, render } from '@testing-library/react'
+import { createRef } from 'react'
+
+import { Select } from './Select'
+
+const generateOptions = (length: number) => {
+	return Array.from({ length }, () => ({
+		label: faker.person.fullName(),
+		value: faker.string.uuid(),
+	}))
+}
+
+describe('Field component', () => {
+	it('renders input correctly', () => {
+		const { getByTestId } = render(
+			<Select data-testid="select-field" options={generateOptions(6)} />,
+		)
+		expect(getByTestId('select-field')).toBeInTheDocument()
+	})
+
+	it('displays warning when isInvalid true', () => {
+		const { getByTestId } = render(
+			<Select
+				data-testid="select-field"
+				options={generateOptions(9)}
+				isInvalid
+			/>,
+		)
+		expect(getByTestId('warning-icon')).toBeInTheDocument()
+	})
+
+	it('forwards ref to input element', () => {
+		const ref = createRef<HTMLSelectElement>()
+		render(
+			<Select
+				data-testid="select-field"
+				options={generateOptions(10)}
+				ref={ref}
+			/>,
+		)
+		expect(ref.current).toBeDefined()
+	})
+
+	it('applies custom class name', () => {
+		const { getByTestId } = render(
+			<Select
+				data-testid="select-field"
+				options={generateOptions(2)}
+				className="custom-class"
+			/>,
+		)
+		const select = getByTestId('select-field')
+		expect(select).toHaveClass('custom-class')
+	})
+
+	it('renders options correctly', () => {
+		const onChangeMock = jest.fn()
+		const randomOption = Math.floor(Math.random() * 10)
+		const OPTIONS = generateOptions(randomOption)
+
+		const { getByTestId } = render(
+			<Select
+				data-testid="select-field"
+				options={OPTIONS}
+				onChange={onChangeMock}
+			/>,
+		)
+
+		const options = getByTestId('select-field').querySelectorAll('option')
+
+		expect(options).toHaveLength(randomOption)
+	})
+
+	it('ref getting value correctly', () => {
+		const onChangeMock = jest.fn()
+		const ref = createRef<HTMLSelectElement>()
+		const OPTIONS = generateOptions(8)
+		const randomOption = Math.floor(Math.random() * OPTIONS.length)
+
+		const { getByTestId } = render(
+			<Select
+				options={OPTIONS}
+				onChange={onChangeMock}
+				ref={ref}
+				data-testid="select-field"
+			/>,
+		)
+
+		const selectOption = OPTIONS[randomOption].value
+
+		const selectElement = getByTestId('select-field')
+
+		fireEvent.change(selectElement, { target: { value: selectOption } })
+
+		expect(ref.current?.value).toBe(selectOption)
+	})
+})
---
+++ src/components/Atoms/Select/Select.tsx
@@ -0,0 +1,66 @@
+import { ComponentProps, forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type SelectProps = ComponentProps<'select'> & {
+	isInvalid?: boolean
+	options: Array<{
+		label: string
+		value: string | number
+	}>
+}
+
+const Warning = () => {
+	return (
+		<div
+			className="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-3"
+			data-testid="warning-icon"
+		>
+			<svg
+				className="size-4 flex-shrink-0 text-red-500"
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<circle cx="12" cy="12" r="10"></circle>
+				<line x1="12" x2="12" y1="8" y2="12"></line>
+				<line x1="12" x2="12.01" y1="16" y2="16"></line>
+			</svg>
+		</div>
+	)
+}
+
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+	({ options, className, isInvalid, ...props }, ref) => {
+		if (!options) return null
+
+		return (
+			<div className="relative">
+				<select
+					className={twMerge(
+						'block w-full rounded-lg px-4 py-3 pe-16 text-sm disabled:pointer-events-none disabled:opacity-50',
+						className,
+						isInvalid &&
+							'border-red-500 focus:border-red-500 focus:ring-red-500',
+					)}
+					ref={ref}
+					{...props}
+				>
+					{options.map(({ label, value }) => (
+						<option key={value} value={value}>
+							{label}
+						</option>
+					))}
+				</select>
+				{isInvalid ? <Warning /> : null}
+			</div>
+		)
+	},
+)
+
+Select.displayName = 'Select'
