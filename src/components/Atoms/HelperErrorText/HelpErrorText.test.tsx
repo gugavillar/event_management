fd652ada1456d95e685ec0aba79b44dc7947fd77
Guga Villar <grsv.21@gmail.com>
2024-06-30 09:16:11 -0300

    :sparkles: HelpErrorText component

---
+++ src/components/Atoms/HelperErrorText/HelpErrorText.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react'
+
+import { HelperErrorText } from './HelperErrorText'
+
+describe('HelperErrorText component', () => {
+	it('renders correctly', () => {
+		const { getByTestId } = render(
+			<HelperErrorText data-testid="helper-text" />,
+		)
+		const helperText = getByTestId('helper-text')
+		expect(helperText).toBeInTheDocument()
+	})
+
+	it('applies custom classes', () => {
+		const { getByTestId } = render(
+			<HelperErrorText data-testid="helper-text" className="custom-class" />,
+		)
+		const helperText = getByTestId('helper-text')
+		expect(helperText).toHaveClass('custom-class')
+	})
+
+	it('applies custom props', () => {
+		const { getByTestId } = render(
+			<HelperErrorText data-testid="helper-text" id="test" />,
+		)
+		const helperText = getByTestId('helper-text')
+		expect(helperText).toHaveAttribute('id', 'test')
+	})
+})
---
+++ src/components/Atoms/HelperErrorText/HelperErrorText.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export const HelperErrorText = ({
+	children,
+	className,
+	...props
+}: ComponentProps<'p'>) => {
+	return (
+		<p className={twMerge('mt-2 text-sm text-red-600', className)} {...props}>
+			{children}
+		</p>
+	)
+}
