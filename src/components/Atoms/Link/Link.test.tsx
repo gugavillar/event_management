a1453f6567d4b7b3e85efc36ca25c11aff575650
Guga Villar <grsv.21@gmail.com>
2024-07-03 20:27:07 -0300

    :sparkles: Link component

---
+++ src/components/Atoms/Link/Link.test.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react'
+
+import { Link } from './Link'
+
+describe('Link component', () => {
+	it('renders correctly', () => {
+		const { getByRole } = render(<Link href="#">Link</Link>)
+
+		const link = getByRole('link', { name: 'Link' })
+
+		expect(link).toBeInTheDocument()
+	})
+
+	it('applies custom classes', () => {
+		const { getByRole } = render(
+			<Link href="#" className="custom-class">
+				Link
+			</Link>,
+		)
+
+		const link = getByRole('link', { name: 'Link' })
+
+		expect(link).toHaveClass('custom-class')
+	})
+
+	it('renders change href', () => {
+		const { getByRole, debug } = render(
+			<Link href="#" as="/link">
+				Link
+			</Link>,
+		)
+
+		const link = getByRole('link', { name: 'Link' })
+		debug(link)
+
+		expect(link).toHaveAttribute('href', '/link')
+	})
+})
---
+++ src/components/Atoms/Link/Link.tsx
@@ -0,0 +1,23 @@
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type LinkProps = NextLinkProps & {
+	className?: string
+	children?: ReactNode
+}
+
+export const Link = ({ href, className, children, ...props }: LinkProps) => {
+	return (
+		<NextLink
+			href={href}
+			className={twMerge(
+				'inline-flex items-center gap-x-3 text-lg font-medium text-gray-800',
+				className,
+			)}
+			{...props}
+		>
+			{children}
+		</NextLink>
+	)
+}
