485d8fd0e6085d577ef01d043d2e69ead9ffb563
Guga Villar <grsv.21@gmail.com>
2025-04-19 15:01:12 -0300

    :recycle: Refact events page

---
+++ src/components/Organisms/CreateEventButton/CreateEventButton.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Dispatch, SetStateAction } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { LuCalendarPlus } from 'react-icons/lu'
+
+import { Button } from '@/components/Atoms'
+import { overlayOpen } from '@/constants'
+import { EventsFromAPI } from '@/services/queries/events/event.type'
+
+import { EventDrawer } from '../EventDrawer'
+import { EventSchema, EventSchemaType } from '../EventDrawer/EventDrawer.schema'
+
+type CreateEventButtonProps = {
+	drawerId: string
+	selectedEvent: null | EventsFromAPI['id']
+	setSelectedEvent: Dispatch<SetStateAction<EventsFromAPI['id'] | null>>
+}
+
+export const CreateEventButton = ({
+	drawerId,
+	selectedEvent,
+	setSelectedEvent,
+}: CreateEventButtonProps) => {
+	const methods = useForm<EventSchemaType>({
+		defaultValues: {
+			name: '',
+			gender: '',
+			initialDate: '',
+			finalDate: '',
+			participantPrice: '',
+			volunteerPrice: '',
+		},
+		mode: 'onChange',
+		resolver: zodResolver(EventSchema),
+	})
+
+	const handleCreateEvent = () => {
+		setSelectedEvent(null)
+		overlayOpen(drawerId)
+	}
+
+	return (
+		<>
+			<Button
+				type="button"
+				onClick={handleCreateEvent}
+				leftIcon={<LuCalendarPlus />}
+				className="min-w-60 items-center justify-center border-transparent bg-teal-500 text-base text-gray-50 transition-colors duration-500 hover:bg-teal-400 hover:text-slate-800"
+			>
+				Criar um novo evento
+			</Button>
+			<FormProvider {...methods}>
+				<EventDrawer
+					drawerId={drawerId}
+					selectedEvent={selectedEvent}
+					setSelectedEvent={setSelectedEvent}
+				/>
+			</FormProvider>
+		</>
+	)
+}
--- src/components/Templates/Events/Events.tsx
+++ src/components/Templates/Events/Events.tsx
@@ -1,21 +1,13 @@
 'use client'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { useState } from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
-import { LuCalendarPlus } from 'react-icons/lu'
 
-import { Button } from '@/components/Atoms'
 import { ListManager } from '@/components/Molecules'
 import {
+	CreateEventButton,
 	EventDeleteModal,
-	EventDrawer,
 	ListPage,
 	PageContent,
 } from '@/components/Organisms'
-import {
-	EventSchema,
-	EventSchemaType,
-} from '@/components/Organisms/EventDrawer/EventDrawer.schema'
 import { MODALS_IDS, overlayOpen } from '@/constants'
 import { useTooltip } from '@/hooks'
 import { useGetEvents } from '@/services/queries/events'
@@ -30,27 +22,10 @@ export const Events = () => {
 
 	const { data, isLoading, search, setSearch } = useGetEvents()
 
-	const methods = useForm<EventSchemaType>({
-		defaultValues: {
-			name: '',
-			gender: '',
-			initialDate: '',
-			finalDate: '',
-			participantPrice: '',
-			volunteerPrice: '',
-		},
-		mode: 'onChange',
-		resolver: zodResolver(EventSchema),
-	})
 	useTooltip(Boolean(selectedEvent))
 
 	const handleEditEvent = (id: EventsFromAPI['id']) => {
 		setSelectedEvent(id)
-		overlayOpen(MODALS_IDS.EVENT_CREATE_OR_UPDATE_DRAWER)
-	}
-
-	const handleCreateEvent = () => {
-		setSelectedEvent(null)
 		overlayOpen(MODALS_IDS.EVENT_CREATE_OR_UPDATE_DRAWER)
 	}
 
@@ -73,14 +48,11 @@ export const Events = () => {
 				search={search}
 				setSearch={setSearch}
 				actionButton={
-					<Button
-						type="button"
-						onClick={handleCreateEvent}
-						leftIcon={<LuCalendarPlus />}
-						className="min-w-60 items-center justify-center border-transparent bg-teal-500 text-base text-gray-50 transition-colors duration-500 hover:bg-teal-400 hover:text-slate-800"
-					>
-						Criar um novo evento
-					</Button>
+					<CreateEventButton
+						drawerId={MODALS_IDS.EVENT_CREATE_OR_UPDATE_DRAWER}
+						selectedEvent={selectedEvent}
+						setSelectedEvent={setSelectedEvent}
+					/>
 				}
 			>
 				<ListManager
@@ -89,13 +61,6 @@ export const Events = () => {
 					isLoading={isLoading}
 				/>
 			</ListPage>
-			<FormProvider {...methods}>
-				<EventDrawer
-					drawerId={MODALS_IDS.EVENT_CREATE_OR_UPDATE_DRAWER}
-					selectedEvent={selectedEvent}
-					setSelectedEvent={setSelectedEvent}
-				/>
-			</FormProvider>
 			<EventDeleteModal
 				modalId={MODALS_IDS.EVENT_REMOVE_MODAL}
 				selectedEvent={selectedEvent}
