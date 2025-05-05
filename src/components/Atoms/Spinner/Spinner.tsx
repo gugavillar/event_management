08a19f88a2661a7c6aab11ae82b3233d7e105289
Guga Villar <grsv.21@gmail.com>
2025-05-05 09:02:34 -0300

    :bug: Fix spinner

--- src/components/Atoms/Spinner/Spinner.tsx
+++ src/components/Atoms/Spinner/Spinner.tsx
@@ -1,21 +1,9 @@
 import { ComponentProps } from 'react'
+import { LiaSpinnerSolid } from 'react-icons/lia'
 import { twMerge } from 'tailwind-merge'
 
 export type SpinnerProps = ComponentProps<'div'>
 
-export const Spinner = ({ className, ...props }: SpinnerProps) => {
-	return (
-		<div
-			className={twMerge(
-				'inline-block size-6 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blue-600 dark:text-blue-500',
-				className,
-			)}
-			role="status"
-			aria-label="loading"
-			data-testid="spinner"
-			{...props}
-		>
-			<span className="sr-only">Loading...</span>
-		</div>
-	)
+export const Spinner = ({ className }: SpinnerProps) => {
+	return <LiaSpinnerSolid className={twMerge('animate-spin', className)} />
 }
