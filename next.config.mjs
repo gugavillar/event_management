e9eba830f1ef925c0fe46d14950d2a332c1e832c
Guga Villar <grsv.21@gmail.com>
2025-06-07 19:41:42 -0300

    :closed_lock_with_key: Env pix

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
 		API_BASE_URL: process.env.API_BASE_URL,
 		API_IBGE_UF: process.env.API_IBGE_UF,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		PIX_KEY: process.env.PIX_KEY,
+		NEXT_PUBLIC_PIX_KEY: process.env.NEXT_PUBLIC_PIX_KEY,
 	},
 }
 
