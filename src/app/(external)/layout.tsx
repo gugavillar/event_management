f11a5c5a38a7afd5703aaf5ed6529ab282b4f763
Guga Villar <grsv.21@gmail.com>
2025-12-29 22:28:53 -0300

    Update layout.tsx

--- src/app/(external)/layout.tsx
+++ src/app/(external)/layout.tsx
@@ -14,7 +14,7 @@ export default function ExternalLayout({
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", ${process.env.CLARITY});`,
+    })(window, document, "clarity", "script", "${process.env.CLARITY}");`,
 					}}
 					id="clarity"
 					strategy="afterInteractive"
