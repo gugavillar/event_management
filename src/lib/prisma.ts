bde6b57865c8777b54e33e9baae7cb9328e88deb
Guga Villar <grsv.21@gmail.com>
2026-01-30 08:20:09 -0300

    :bug: Connection database error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,18 @@
 import 'dotenv/config'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
-import { IS_NOT_DEVELOPMENT } from '@/constants'
-
 import { PrismaClient } from '../../prisma/prisma/generate/prisma/client'
 
-const adapter = new PrismaMariaDb(
-	IS_NOT_DEVELOPMENT
-		? {
-				connectionLimit: 5,
-				database: process.env.MYSQL_DATABASE,
-				host: process.env.MYSQL_HOST,
-				password: process.env.MYSQL_PASSWORD,
-				user: process.env.MYSQL_USER,
-			}
-		: (process.env.DATABASE_URL as string)
-)
+const globalForPrisma = globalThis as unknown as {
+	prisma?: PrismaClient
+}
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL as string)
+
 const prisma = new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.prisma = prisma
+}
 
 export { prisma }
