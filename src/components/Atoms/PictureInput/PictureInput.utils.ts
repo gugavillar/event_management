4d5a243923a3001289e32c6fab977c6d657ce9e4
Guga Villar <grsv.21@gmail.com>
2026-03-16 21:34:06 -0300

    ✨ Force file type and quality

--- src/components/Atoms/PictureInput/PictureInput.utils.ts
+++ src/components/Atoms/PictureInput/PictureInput.utils.ts
@@ -2,6 +2,8 @@ import imageCompression from 'browser-image-compression'
 
 export const compressImage = async (file: File) => {
 	const options = {
+		fileType: 'image/jpeg',
+		initialQuality: 0.8,
 		maxSizeMB: 0.2, // máximo 200KB
 		maxWidthOrHeight: 1280,
 		useWebWorker: true,
