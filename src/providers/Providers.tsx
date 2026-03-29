b21b54d60d0739b42cf7a833ab482d4b5b8d776b
Guga Villar <grsv.21@gmail.com>
2026-03-29 15:15:10 -0300

    ✨ Add close button in toast

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { XCircle } from 'lucide-react'
 import dynamic from 'next/dynamic'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import type { ReactNode } from 'react'
-import { Toaster } from 'react-hot-toast'
+import toast, { ToastBar, Toaster } from 'react-hot-toast'
 
 import { QueryProvider } from './QueryProvider'
 
@@ -24,7 +25,24 @@ export const Providers = ({ children }: Readonly<{ children: ReactNode }>) => {
 						maxWidth: '25rem',
 					},
 				}}
-			/>
+			>
+				{(t) => (
+					<ToastBar toast={t}>
+						{({ icon, message }) => (
+							<div className="relative flex w-full items-center gap-3">
+								{icon}
+								<div className="flex-1">{message}</div>
+								<button
+									className="absolute -top-3.5 -right-3.5 cursor-pointer text-gray-500"
+									onClick={() => toast.dismiss(t.id)}
+								>
+									<XCircle className="size-5" />
+								</button>
+							</div>
+						)}
+					</ToastBar>
+				)}
+			</Toaster>
 		</SidebarProvider>
 	)
 }
